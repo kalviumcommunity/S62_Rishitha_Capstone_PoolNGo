d28231d95387182c6c021fedd96ade2e0171d17d
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-03-26 15:18:52 +0530

    Deployed backend server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-require("dotenv").config()
+const express = require("express");
+require("dotenv").config();
 const connectDatabase = require("./DB/database");
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const driverRoutes = require('./routes/driverRoutes');
-const cabSlotRoutes = require('./routes/cabSlotRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const cors = require('cors');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const driverRoutes = require("./routes/driverRoutes");
+const cabSlotRoutes = require("./routes/cabSlotRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const cors = require("cors");
 
 const app = express();
 
@@ -20,10 +20,13 @@ app.use(driverRoutes);
 app.use(cabSlotRoutes);
 app.use(bookingRoutes);
 
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome to the backend");
+});
 // Connect to the database
 connectDatabase();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
